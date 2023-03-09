60800c96f39761ce67171b2897147d59bb7af640
Diego Urbina <diego.urbina@acceso.com.pe>
2023-03-09 16:10:21 -0500

    Fix

--- src/app.ts
+++ src/app.ts
@@ -1,16 +1,15 @@
 import dotenv from 'dotenv';
 import { server } from './server/infrastructure/dependencies/server.dependency';
-
-const log = require('./libs/logs/winston');
+import { log } from './libs/logs/winston';
 
 dotenv.config({ path: `.env.${process.env.NODE_ENV}` });
 
 const host: string = process.env.HOST || '0.0.0.0';
-const port: string = process.env.PORT || '3001';
+const port: string = process.env.PORT || '3000';
 
 try {
   server.run(host, port);
 } catch (error) {
-  console.error(error);
+  log.error(error);
   process.exit(1);
 }
