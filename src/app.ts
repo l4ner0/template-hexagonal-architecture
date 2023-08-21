f5f30da3ab190e948d897c22f75c2cc7cd9a5101
l4ner0 <diegourbina1284@gmail.com>
2023-08-21 05:05:53 +0000

    Add sentry in app.ts

--- src/app.ts
+++ src/app.ts
@@ -1,7 +1,7 @@
 import dotenv from 'dotenv';
 import { WinstonAdapter } from './libs/log/winston.adapter';
 import { ApplicationServer } from './server/application-server';
-import * as Sentry from '@sentry/node'
+import * as sentry from '@sentry/node'
 
 dotenv.config({ path: `.env.${process.env.NODE_ENV}` });
 
@@ -11,7 +11,7 @@ const log = new WinstonAdapter();
 // or use ESM import statements
 // import * as Sentry from '@sentry/node';
 
-Sentry.init({
+sentry.init({
   dsn: "https://458869cf1e298b7de929a4be5da1c627@o4504001157267456.ingest.sentry.io/4505740874940416",
   // Performance Monitoring
   tracesSampleRate: 1.0, // Capture 100% of the transactions, reduce in production!
@@ -38,6 +38,7 @@ const port: string = process.env.PORT || '3000';
 try {
   new ApplicationServer().run(host, port);
 } catch (error: Error | unknown) {
+  sentry.captureException(error);
   if (error instanceof Error) {
     log.error(error.message);
   }
